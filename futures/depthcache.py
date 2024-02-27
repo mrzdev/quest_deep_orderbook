04a3834c10d8671c624e08127ed5570ddeba2ac0
mrzdev <mr85291@stud.uph.edu.pl>
2024-02-27 08:42:32 +0100

    :recycle: Use threaded depth cache manager

--- futures/depthcache.py
+++ futures/depthcache.py
@@ -1,12 +1,26 @@
-from binance.depthcache import FuturesDepthCacheManager
+from typing import Callable
+from binance.depthcache import FuturesDepthCacheManager, ThreadedDepthCacheManager
 
 class FDCManager(FuturesDepthCacheManager):
-    """Inherit from FuturesDepthCacheManager class and provide a way to specify the depth argument."""
-    def _get_socket(self, depth: str = ''):
+    """Inherit from FuturesDepthCacheManager class, specifying depth argument."""
+    def _get_socket(self):
         """
-        Subscribe to a futures depth data stream, specifying the depth argument.
-        :param depth: optional Number of depth entries to return, default '' meaning unlimited
-        :type depth: str
+        Subscribe to a futures depth data stream.
         """
-        sock = self._bm.futures_depth_socket(symbol=self._symbol, depth = depth)
+        sock = self._bm.futures_depth_socket(symbol=self._symbol, depth = self._limit)
         return sock
+
+class ThreadedFDCManager(ThreadedDepthCacheManager):
+    """Pass FDCManager on start_futures_depth_socket call."""
+    def start_futures_depth_socket(
+            self, callback: Callable, symbol: str, refresh_interval=None, bm=None, limit=10, conv_type=float
+    ) -> str:
+        return self._start_depth_cache(
+            dcm_class=FDCManager,
+            callback=callback,
+            symbol=symbol,
+            refresh_interval=refresh_interval,
+            bm=bm,
+            limit=limit,
+            conv_type=conv_type
+        )
